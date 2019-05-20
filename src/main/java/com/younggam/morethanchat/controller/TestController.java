2df368e6577da1d082f7ac80c519a2b5fda3f4bd
mjung1798 <mor2222@naver.com>
2019-05-20 14:16:58 +0900

    make test api Response

--- src/main/java/com/younggam/morethanchat/controller/TestController.java
+++ src/main/java/com/younggam/morethanchat/controller/TestController.java
@@ -1,11 +1,30 @@
 package com.younggam.morethanchat.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("test")
 public class TestController {
 
+    @GetMapping
+    public ResponseEntity getTest() {
+        return new ResponseEntity("test api [get] 호출에 성공했습니다.", HttpStatus.OK);
+    }
 
-    private String name;
+    @PostMapping
+    public ResponseEntity postTest() {
+        return new ResponseEntity("test api [post] 호출에 성공했습니다.", HttpStatus.OK);
+    }
 
+    @PutMapping
+    public ResponseEntity putTest() {
+        return new ResponseEntity("test api [put] 호출에 성공했습니다.", HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteTest() {
+        return new ResponseEntity("test api [delete] 호출에 성공했습니다.", HttpStatus.OK);
+    }
 }
